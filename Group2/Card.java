e44b1d7d96a0ba8d0467866ddc510002d555a6e0
JerryChenSHS <jechen8@students.d125.org>
2016-03-11 10:48:20 -0600

    bullshit

---
+++ Group2/Card.java
@@ -0,0 +1,47 @@
+public class Card implements Comparable<Card>
+{
+    private int Id;
+    public void Card(int newid)
+
+    {
+        Id = newid;
+    }
+
+    public int getId()
+    {
+        return Id;
+    }
+
+    public boolean equals(Card other)
+    {
+        if (other.getId() == getId())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int compareTo(Card other)
+    {
+        if (getId() == other.getId())
+        {
+            return 0;
+        }
+        else if (getId() > other.getId())
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public String toString()
+    {
+         return Integer.toString(getId());
+    }
+}

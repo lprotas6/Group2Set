1b7c1b557ddc807c96a47ec8f3f0531ad859d4a2
rloundy6 <rloundy6@students.d125.org>
2016-03-16 10:08:47 -0500

    status

--- Group1/Card.java
+++ Group1/Card.java
@@ -3,12 +3,10 @@ public class Card implements Comparable<Card>
     private int Id;
     
 
-<<<<<<< HEAD
-    public void Card(int id)
-=======
+
+ 
     public Card(int newid)
 
->>>>>>> 9e0a79610d7a089a9a3ea96ef5a2bc69ccfde57a
 
     {
         Id = id;
